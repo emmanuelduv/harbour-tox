6b4a268aa35808ead8e1a0ee980a34cb2a940e18
emmanuelduv <emmanuelduviviers49@hotmail.com>
2015-06-11 18:36:22 +0200

    Coherent names for translations

---
+++ translations/harbour-tox-fr.ts
@@ -0,0 +1,238 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>ContactAdd</name>
+    <message>
+        <source>Create contact</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>tox ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hello, I would like to tox with you!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invitation message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Send message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Send a invitation message</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ContactWrite</name>
+    <message>
+        <source>Send</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hello!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Nothing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Header</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Conversation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You :</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation type="vanished">Miniature</translation>
+    </message>
+    <message>
+        <source>My Tox</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Show Page 2</source>
+        <translation type="vanished">Page 2</translation>
+    </message>
+    <message>
+        <source>UI Template</source>
+        <translation type="vanished">Modèle UI</translation>
+    </message>
+    <message>
+        <source>Hello Sailors</source>
+        <translation type="vanished">Bonjour sailors</translation>
+    </message>
+    <message>
+        <source>Delete contacts</source>
+        <translation>Supprimer les contacts</translation>
+    </message>
+    <message>
+        <source>Tox</source>
+        <translation>Tox</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Supprimer</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Suppression</translation>
+    </message>
+    <message>
+        <source>Copy to clipboard</source>
+        <translation>Copier</translation>
+    </message>
+    <message>
+        <source>No contacts</source>
+        <translation>Aucun contact</translation>
+    </message>
+    <message>
+        <source>Add contact</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Send message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Nested Page</source>
+        <translation type="vanished">Sous page</translation>
+    </message>
+    <message>
+        <source>Item</source>
+        <translation type="vanished">Element</translation>
+    </message>
+    <message>
+        <source>tox ID</source>
+        <translation>ToxId</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation>Statut</translation>
+    </message>
+    <message>
+        <source>Audio</source>
+        <translation>Audio</translation>
+    </message>
+    <message>
+        <source>No audio</source>
+        <translation>Pas de son</translation>
+    </message>
+    <message>
+        <source>Loudspeaker</source>
+        <translation>Haut parleurx</translation>
+    </message>
+    <message>
+        <source>Earphone</source>
+        <translation>Ecouteur</translation>
+    </message>
+    <message>
+        <source>Video</source>
+        <translation>Vidéo</translation>
+    </message>
+    <message>
+        <source>No vidéo</source>
+        <translation>Pas de vidéo</translation>
+    </message>
+    <message>
+        <source>Front camera 176x144</source>
+        <translation>Caméra frontale 176x144</translation>
+    </message>
+    <message>
+        <source>Front camera 320x240</source>
+        <translation>Caméra frontale 320x240</translation>
+    </message>
+    <message>
+        <source>Front camera native</source>
+        <translation>Caméra frontale res. native</translation>
+    </message>
+    <message>
+        <source>Back camera 176x144</source>
+        <translation>Caméra arrière 176x144</translation>
+    </message>
+    <message>
+        <source>Back camera 320x240</source>
+        <translation>Caméra arrière 320x240</translation>
+    </message>
+    <message>
+        <source>Back camera native</source>
+        <translation>Caméra arrière res. native</translation>
+    </message>
+    <message>
+        <source>Proxy</source>
+        <translation>Proxy</translation>
+    </message>
+    <message>
+        <source>Port</source>
+        <translation>Port</translation>
+    </message>
+    <message>
+        <source>Valider</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>multimedia_settings</name>
+    <message>
+        <source>tox ID</source>
+        <translation type="obsolete">Tox ID</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="obsolete">Nom</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation type="obsolete">Statut</translation>
+    </message>
+    <message>
+        <source>Audio</source>
+        <translation type="vanished">Audio</translation>
+    </message>
+    <message>
+        <source>No audio</source>
+        <translation type="vanished">Aucun</translation>
+    </message>
+    <message>
+        <source>Loudspeaker</source>
+        <translation type="vanished">Haut parleur</translation>
+    </message>
+    <message>
+        <source>Earphone</source>
+        <translation type="vanished">Ecouteur</translation>
+    </message>
+    <message>
+        <source>Video</source>
+        <translation type="vanished">Vidéo</translation>
+    </message>
+</context>
+</TS>
